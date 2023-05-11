7fc17a13bcdfd91f8a5d049f29fbb68a10678120
Wendy Wang <wendywangg928@gmail.com>
2023-05-11 14:51:47 -0400

    updated but still incorrect

--- index.js
+++ index.js
@@ -10,30 +10,59 @@ let waterIsOn = true
 //Everything below is fair game for you to power on the rides
 
 //Ride 1 works when:
-    // green wire is connected
+// green wire is connected
 ride1PowerOn = false
+if (greenWireConnected) {
+    ride1PowerOn = true
+}
 
 //Ride 2 works when 
-    //green and yellow wires are connected
+//green and yellow wires are connected
 ride2PowerOn = false
-
+if (greenWireConnected && yellowWireConnected) {
+    ride2PowerOn = true
+}
 //Ride 3 works when:
-    // green and red wires are connected
-    // water is on 
-    // yellow wire is not connected
+// green and red wires are connected
+// water is on 
+// yellow wire is not connected
 ride3PowerOn = false
-
+if (greenWireConnected && redWireConnected && waterIsOn) {
+    ride3PowerOn = true
+}
 //Ride 4 has power when:
-    //ride wire is connected
+//red wire is connected
 ride4PowerOn = false
+if (redWireConnected && !yellowWireConnected){
+    ride4PowerOn = true;
+}
+
 //Ride 4 is a slot machine
-    //if a 1 is rolled it should return "star"
-    //if a 2 is rolled it should return "car"
-    //if a 3 is rolled it should return "pot of gold"
-    //if a 4 is rolled it should return "clover"
-    //if a 5 is rolled it should return "bowling ball"
-    //if a 6 is rolled it should return "tiger"
-    //if a 7 is rolled it should return "seven"
-function ride4SlotMachine(rolledNumber){
+switch(rolledNumber){
+    case "1" :
+        console.log("star")
+    case "2" :
+        console.log("car")
+    case "3" :
+        console.log("pot of gold")
+    case "4" :
+        console.log("clover")
+    case "5" :
+        console.log("bowling ball")
+    case "6" :
+        console.log("tiger")
+    case "7" :
+        console.log("seven")
+    default:
+        break;
+}
+//if a 1 is rolled it should return "star"
+//if a 2 is rolled it should return "car"
+//if a 3 is rolled it should return "pot of gold"
+//if a 4 is rolled it should return "clover"
+//if a 5 is rolled it should return "bowling ball"
+//if a 6 is rolled it should return "tiger"
+//if a 7 is rolled it should return "seven"
+function ride4SlotMachine(rolledNumber) {
     return "nothing"
 }
